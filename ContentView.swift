23253fa8f244dfb40b2b571981a01a3091bb638c
Taegyu <mordol@gmail.com>
2025-07-07 13:47:35 +0900

    Display running applications in settings

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var runningApps = NSWorkspace.shared.runningApplications
+    @State private var selectedApp: NSRunningApplication?
+
     var body: some View {
         VStack {
             Text("ControlTabMapper Settings")
@@ -10,9 +13,29 @@ struct ContentView: View {
 
             Text("Select an application to enable Control-Tab mapping.")
 
-            // A list of running applications will be displayed here.
+            List(runningApps, id: \.bundleIdentifier) { app in
+                Button(action: {
+                    selectedApp = app
+                }) {
+                    HStack {
+                        if let icon = app.icon {
+                            Image(nsImage: icon)
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                        }
+                        Text(app.localizedName ?? "Unknown App")
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                .background(app.bundleIdentifier == selectedApp?.bundleIdentifier ? Color.accentColor : Color.clear)
+                .cornerRadius(5)
+            }
         }
         .padding()
+        .onAppear {
+            // Filter to only show apps with a UI
+            runningApps = runningApps.filter { $0.activationPolicy == .regular }
+        }
     }
 }
 
